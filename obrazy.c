6dfae8420830ba1682cd754728c4636c990c81bf
CrassusXY <75444051+CrassusXY@users.noreply.github.com>
2020-12-09 17:15:10 +0100

    Update obrazy.c

--- obrazy.c
+++ obrazy.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define MAX 512            /* Maksymalny rozmiar wczytywanego obrazu */
+#define MAX 2048            /* Maksymalny rozmiar wczytywanego obrazu */
 #define DL_LINII 1024      /* Dlugosc buforow pomocniczych */
 
 /************************************************************************************
